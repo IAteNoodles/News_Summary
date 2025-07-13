8fede502646785d227680f520a967a98b1cad8a7
Abhijit <thisabhijithere@gmail.com>
2025-07-13 21:28:54 +0000

    Configured JWT

--- api/views.py
+++ api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import UserRegistrationSerializer
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
+
+class UserRegistrationView(generics.CreateAPIView):
+    """
+    An endpoint for registering a new user.
+    """
+    serializer_class = UserRegistrationSerializer
+    permission_classes = (AllowAny,)
